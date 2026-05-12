579644e0bba37f83acd3db33441ace2e1cd819d6
Zash Official <bsash017@gmail.com>
2026-05-12 12:02:01 +0500

    static fix

--- server-groq.js
+++ server-groq.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./'));
+app.use(express.static(__dirname));
 
 app.post('/chat', async (req, res) => {
   try {
